6b11124377b9e802cc1674dda308744ded1abda7
M05 <pre.mos_os@hotmail.com>
2016-09-09 17:47:28 +0700

    create page !!

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'ionic-material', 'ionMdInput'])
+angular.module('starter', ['ionic', 'ionic-material', 'starter.controllers'])
 
 .run(function($ionicPlatform) {
     $ionicPlatform.ready(function() {
@@ -20,109 +20,78 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ionic-material', 'io
     });
 })
 
-.config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
+.config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
 
-    // Turn off caching for demo simplicity's sake
-    $ionicConfigProvider.views.maxCache(0);
-
-    /*
-    // Turn off back button text
-    $ionicConfigProvider.backButton.previousTitleText(false);
-    */
-
-    $stateProvider.state('app', {
+    .state('app', {
         url: '/app',
         abstract: true,
         templateUrl: 'templates/menu.html',
         controller: 'AppCtrl'
     })
 
-    .state('app.activity', {
-        url: '/activity',
+    .state('app.lists', {
+        url: '/lists',
         views: {
             'menuContent': {
-                templateUrl: 'templates/activity.html',
-                controller: 'ActivityCtrl'
-            },
-            'fabContent': {
-                template: '<button id="fab-activity" class="button button-fab button-fab-top-right expanded button-energized-900 flap"><i class="icon ion-paper-airplane"></i></button>',
-                controller: function ($timeout) {
-                    $timeout(function () {
-                        document.getElementById('fab-activity').classList.toggle('on');
-                    }, 200);
-                }
+                templateUrl: 'templates/lists.html',
+                controller: 'ListsCtrl'
             }
         }
     })
 
-    .state('app.friends', {
-        url: '/friends',
+    .state('app.ink', {
+        url: '/ink',
         views: {
             'menuContent': {
-                templateUrl: 'templates/friends.html',
-                controller: 'FriendsCtrl'
-            },
-            'fabContent': {
-                template: '<button id="fab-friends" class="button button-fab button-fab-top-left expanded button-energized-900 spin"><i class="icon ion-chatbubbles"></i></button>',
-                controller: function ($timeout) {
-                    $timeout(function () {
-                        document.getElementById('fab-friends').classList.toggle('on');
-                    }, 900);
-                }
+                templateUrl: 'templates/ink.html',
+                controller: 'InkCtrl'
             }
         }
     })
 
-    .state('app.gallery', {
-        url: '/gallery',
+    .state('app.motion', {
+        url: '/motion',
         views: {
             'menuContent': {
-                templateUrl: 'templates/gallery.html',
-                controller: 'GalleryCtrl'
-            },
-            'fabContent': {
-                template: '<button id="fab-gallery" class="button button-fab button-fab-top-right expanded button-energized-900 drop"><i class="icon ion-heart"></i></button>',
-                controller: function ($timeout) {
-                    $timeout(function () {
-                        document.getElementById('fab-gallery').classList.toggle('on');
-                    }, 600);
-                }
+                templateUrl: 'templates/motion.html',
+                controller: 'MotionCtrl'
             }
         }
     })
 
-    .state('app.login', {
-        url: '/login',
+    .state('app.components', {
+        url: '/components',
         views: {
             'menuContent': {
-                templateUrl: 'templates/login.html',
-                controller: 'LoginCtrl'
-            },
-            'fabContent': {
-                template: ''
+                templateUrl: 'templates/components.html',
+                controller: 'ComponentsCtrl'
             }
         }
     })
 
-    .state('app.profile', {
-        url: '/profile',
+    .state('app.extensions', {
+        url: '/extensions',
         views: {
             'menuContent': {
-                templateUrl: 'templates/profile.html',
-                controller: 'ProfileCtrl'
-            },
-            'fabContent': {
-                template: '<button id="fab-profile" class="button button-fab button-fab-bottom-right button-energized-900"><i class="icon ion-plus"></i></button>',
-                controller: function ($timeout) {
-                    /*$timeout(function () {
-                        document.getElementById('fab-profile').classList.toggle('on');
-                    }, 800);*/
-                }
+                templateUrl: 'templates/extensions.html',
+                controller: 'ExtensionsCtrl'
             }
         }
     })
+
+    .state('app.Home', {
+        url: '/Home',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/Home.html',
+                controller: 'HomeCtrl'
+            }
+        }
+    })
+
     ;
 
     // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/app/login');
+    $urlRouterProvider.otherwise('/app/Home');
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,178 +1,209 @@
 /* global angular, document, window */
-'use strict';
-
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $ionicPopover, $timeout) {
+.controller('AppCtrl', function($scope, $ionicModal, $ionicPopover, $timeout,  $location) {
     // Form data for the login modal
     $scope.loginData = {};
-    $scope.isExpanded = false;
-    $scope.hasHeaderFabLeft = false;
-    $scope.hasHeaderFabRight = false;
 
     var navIcons = document.getElementsByClassName('ion-navicon');
     for (var i = 0; i < navIcons.length; i++) {
-        navIcons.addEventListener('click', function() {
+        navIcons[i].addEventListener('click', function() {
             this.classList.toggle('active');
         });
     }
 
-    ////////////////////////////////////////
-    // Layout Methods
-    ////////////////////////////////////////
 
-    $scope.hideNavBar = function() {
-        document.getElementsByTagName('ion-nav-bar')[0].style.display = 'none';
+
+    // .fromTemplate() method
+    var template =  '<ion-popover-view>' +
+                    '   <ion-header-bar>' +
+                    '       <h1 class="title">My Popover Title</h1>' +
+                    '   </ion-header-bar>' +
+                    '   <ion-content class="padding">' +
+                    '       My Popover Contents' +
+                    '   </ion-content>' +
+                    '</ion-popover-view>';
+
+    $scope.popover = $ionicPopover.fromTemplate(template, {
+        scope: $scope
+    });
+    $scope.closePopover = function() {
+        $scope.popover.hide();
     };
+    //Cleanup the popover when we're done with it!
+    $scope.$on('$destroy', function() {
+        $scope.popover.remove();
+    });
+})
 
-    $scope.showNavBar = function() {
-        document.getElementsByTagName('ion-nav-bar')[0].style.display = 'block';
-    };
+.controller('InkCtrl', function($scope, $stateParams, ionicMaterialInk) {
+    ionicMaterialInk.displayEffect();
+})
 
-    $scope.noHeader = function() {
-        var content = document.getElementsByTagName('ion-content');
-        for (var i = 0; i < content.length; i++) {
-            if (content[i].classList.contains('has-header')) {
-                content[i].classList.toggle('has-header');
+.controller('ComponentsCtrl', function($scope, $stateParams,  ionicMaterialInk) {
+    ionicMaterialInk.displayEffect();
+
+    // Toggle Code Wrapper
+    var code = document.getElementsByClassName('code-wrapper');
+    for (var i = 0; i < code.length; i++) {
+        code[i].addEventListener('click', function() {
+            this.classList.toggle('active');
+        });
+    }
+})
+.controller('HomeCtrl', function($scope, $stateParams,  ionicMaterialInk) {
+  ionicMaterialInk.displayEffect();
+})
+
+.controller('ListsCtrl', function($scope, $stateParams,  ionicMaterialMotion) {
+
+    var reset = function() {
+        var inClass = document.querySelectorAll('.in');
+        for (var i = 0; i < inClass.length; i++) {
+            inClass[i].classList.remove('in');
+            inClass[i].removeAttribute('style');
+        }
+        var done = document.querySelectorAll('.done');
+        for (var j = 0; j < done.length; j++) {
+            done[j].classList.remove('done');
+            done[j].removeAttribute('style');
+        }
+        var ionList = document.getElementsByTagName('ion-list');
+        for (var k = 0; k < ionList.length; k++) {
+            var toRemove = ionList[k].className;
+            if (/animate-/.test(toRemove)) {
+                ionList[k].className = ionList[k].className.replace(/(?:^|\s)animate-\S*(?:$|\s)/, '');
             }
         }
     };
 
-    $scope.setExpanded = function(bool) {
-        $scope.isExpanded = bool;
+    $scope.ripple = function() {
+        reset();
+        document.getElementsByTagName('ion-list')[0].className += ' animate-ripple';
+        setTimeout(function() {
+            ionicMaterialMotion.ripple();
+        }, 500);
     };
 
-    $scope.setHeaderFab = function(location) {
-        var hasHeaderFabLeft = false;
-        var hasHeaderFabRight = false;
-
-        switch (location) {
-            case 'left':
-                hasHeaderFabLeft = true;
-                break;
-            case 'right':
-                hasHeaderFabRight = true;
-                break;
-        }
-
-        $scope.hasHeaderFabLeft = hasHeaderFabLeft;
-        $scope.hasHeaderFabRight = hasHeaderFabRight;
+    $scope.fadeSlideInRight = function() {
+        reset();
+        document.getElementsByTagName('ion-list')[0].className += ' animate-fade-slide-in-right';
+        setTimeout(function() {
+            ionicMaterialMotion.fadeSlideInRight();
+        }, 500);
     };
 
-    $scope.hasHeader = function() {
-        var content = document.getElementsByTagName('ion-content');
-        for (var i = 0; i < content.length; i++) {
-            if (!content[i].classList.contains('has-header')) {
-                content[i].classList.toggle('has-header');
-            }
-        }
-
+    $scope.fadeSlideIn = function() {
+        reset();
+        document.getElementsByTagName('ion-list')[0].className += ' animate-fade-slide-in';
+        setTimeout(function() {
+            ionicMaterialMotion.fadeSlideIn();
+        }, 500);
     };
 
-    $scope.hideHeader = function() {
-        $scope.hideNavBar();
-        $scope.noHeader();
+    $scope.blinds = function() {
+        reset();
+        document.getElementsByTagName('ion-list')[0].className += ' animate-blinds';
+        setTimeout(function() {
+            ionicMaterialMotion.blinds();
+        }, 500);
     };
 
-    $scope.showHeader = function() {
-        $scope.showNavBar();
-        $scope.hasHeader();
-    };
+    $scope.blinds();
 
-    $scope.clearFabs = function() {
-        var fabs = document.getElementsByClassName('button-fab');
-        if (fabs.length && fabs.length > 1) {
-            fabs[0].remove();
-        }
-    };
 })
 
-.controller('LoginCtrl', function($scope, $timeout, $stateParams, ionicMaterialInk) {
-    $scope.$parent.clearFabs();
-    $timeout(function() {
-        $scope.$parent.hideHeader();
-    }, 0);
-    ionicMaterialInk.displayEffect();
-})
-
-.controller('FriendsCtrl', function($scope, $stateParams, $timeout, ionicMaterialInk, ionicMaterialMotion) {
-    // Set Header
-    $scope.$parent.showHeader();
-    $scope.$parent.clearFabs();
-    $scope.$parent.setHeaderFab('left');
-
-    // Delay expansion
-    $timeout(function() {
-        $scope.isExpanded = true;
-        $scope.$parent.setExpanded(true);
-    }, 300);
-
-    // Set Motion
-    ionicMaterialMotion.fadeSlideInRight();
-
-    // Set Ink
-    ionicMaterialInk.displayEffect();
-})
-
-.controller('ProfileCtrl', function($scope, $stateParams, $timeout, ionicMaterialMotion, ionicMaterialInk) {
-    // Set Header
-    $scope.$parent.showHeader();
-    $scope.$parent.clearFabs();
-    $scope.isExpanded = false;
-    $scope.$parent.setExpanded(false);
-    $scope.$parent.setHeaderFab(false);
-
-    // Set Motion
-    $timeout(function() {
-        ionicMaterialMotion.slideUp({
-            selector: '.slide-up'
-        });
-    }, 300);
-
-    $timeout(function() {
-        ionicMaterialMotion.fadeSlideInRight({
-            startVelocity: 3000
-        });
-    }, 700);
-
-    // Set Ink
-    ionicMaterialInk.displayEffect();
-})
-
-.controller('ActivityCtrl', function($scope, $stateParams, $timeout, ionicMaterialMotion, ionicMaterialInk) {
-    $scope.$parent.showHeader();
-    $scope.$parent.clearFabs();
-    $scope.isExpanded = true;
-    $scope.$parent.setExpanded(true);
-    $scope.$parent.setHeaderFab('right');
-
-    $timeout(function() {
-        ionicMaterialMotion.fadeSlideIn({
-            selector: '.animate-fade-slide-in .item'
-        });
-    }, 200);
-
-    // Activate ink for controller
-    ionicMaterialInk.displayEffect();
-})
-
-.controller('GalleryCtrl', function($scope, $stateParams, $timeout, ionicMaterialInk, ionicMaterialMotion) {
-    $scope.$parent.showHeader();
-    $scope.$parent.clearFabs();
-    $scope.isExpanded = true;
-    $scope.$parent.setExpanded(true);
-    $scope.$parent.setHeaderFab(false);
-
-    // Activate ink for controller
-    ionicMaterialInk.displayEffect();
-
-    ionicMaterialMotion.pushDown({
+.controller('SetupCtrl', function($scope, $stateParams) {
+    /* ionicMaterialMotion.pushDown({
         selector: '.push-down'
     });
-    ionicMaterialMotion.fadeSlideInRight({
-        selector: '.animate-fade-slide-in .item'
-    });
-
+    */
 })
 
-;
+.controller('ExtensionsCtrl', function($scope, $stateParams, $ionicActionSheet, $timeout, $ionicLoading, $ionicModal, $ionicPopup,  ionicMaterialInk) {
+
+    // Triggered on a button click, or some other target
+    $scope.actionSheet = function() {
+
+        // Show the action sheet
+        var hideSheet = $ionicActionSheet.show({
+            buttons: [{
+                text: '<b>Share</b> This'
+            }, {
+                text: 'Move'
+            }],
+            destructiveText: 'Delete',
+            titleText: 'Modify your album',
+            cancelText: 'Cancel',
+            cancel: function() {
+                // add cancel code..
+            },
+            buttonClicked: function(index) {
+                return true;
+            }
+        });
+
+        // For example's sake, hide the sheet after two seconds
+        $timeout(function() {
+            hideSheet();
+        }, 2000);
+
+    };
+
+    $scope.loading = function() {
+        $ionicLoading.show({
+            template: '<div class="loader"><svg class="circular"><circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10"/></svg></div>'
+        });
+
+        // For example's sake, hide the sheet after two seconds
+        $timeout(function() {
+            $ionicLoading.hide();
+        }, 2000);
+    };
+
+    $ionicModal.fromTemplateUrl('my-modal.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+    }).then(function(modal) {
+        $scope.modal = modal;
+    });
+
+    $scope.openModal = function() {
+        $scope.modal.show();
+        $timeout(function () {
+            $scope.modal.hide();
+        }, 2000);
+    };
+    // Cleanup the modal when we're done with it
+    $scope.$on('$destroy', function() {
+        $scope.modal.remove();
+    });
+
+    // Popover
+    $scope.popover = function() {
+        $scope.$parent.popover.show();
+        $timeout(function () {
+            $scope.$parent.popover.hide();
+        }, 2000);
+    };
+
+    // Confirm
+    $scope.showPopup = function() {
+        var alertPopup = $ionicPopup.alert({
+            title: 'You are now my subscribed to Cat Facts',
+            template: 'You will meow receive fun daily facts about CATS!'
+        });
+
+        $timeout(function() {
+            ionicMaterialInk.displayEffect();
+        }, 0);
+    };
+
+    // Toggle Code Wrapper
+    var code = document.getElementsByClassName('code-wrapper');
+    for (var i = 0; i < code.length; i++) {
+        code[i].addEventListener('click', function() {
+            this.classList.toggle('active');
+        });
+    }
+})
